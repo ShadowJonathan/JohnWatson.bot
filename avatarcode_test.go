b202f6095b92b18754103decdd6dc0943892dc69
ShadowJonathan <jonathandejong02@gmail.com>
2017-02-27 13:48:57 +0100

    shit

--- avatarcode_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestEncodefile2(t *testing.T) {
-	Encodefile("Sherlock.jpg","Encoded.png")
-}
--- util.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func HE(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
